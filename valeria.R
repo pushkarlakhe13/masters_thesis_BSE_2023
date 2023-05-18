b96fb96656a54d35591bc7a0c9833b69144b0a58
Valeria Strusi <valeria.strusi@gmail.com>
2023-05-18 18:33:36 +0200

    Added population, vote_share_diff, and ordinal education

--- valeria.R
+++ valeria.R
@@ -13,7 +13,7 @@ census_01 <- read.csv("data/shrug-v1.5.samosa-pop-econ-census-csv/shrug_pc01.csv
 census_11 <- read.csv("data/shrug-v1.5.samosa-pop-econ-census-csv/shrug_pc11.csv")
 
 df <- df %>%
-  filter(Year == 1996 | Year == 1999 | Year == 2004 | Year == 2009 | Year == 2014)
+  filter(Year == 1996 | Year == 1999 | Year == 2004 | Year == 2009)
 
 nl <- merge(nl, key[, c("shrid", "pc11_district_name")], by = "shrid")
 nl <- merge(nl, census_91[, c("shrid", "pc91_pca_tot_p")], by = "shrid")
@@ -26,6 +26,7 @@ nl <- nl %>%
          total_light1999, total_light_cal1999, max_light1999,
          total_light2004, total_light_cal2004, max_light2004,
          total_light2009, total_light_cal2009, max_light2009,
+         total_light2013, total_light_cal2013, max_light2013,
          pc91_pca_tot_p, pc01_pca_tot_p, pc11_pca_tot_p) %>%
   filter(!is.na(pc11_district_name))
 
@@ -41,3 +42,92 @@ df <- df %>%
   filter(!is.na(total_light1996) & (Position == 1 | Position == 2)) %>%
   arrange(district)
 
+# Define a custom function
+predict_population <- function(y1, y2, y3, x1 = 1991, x2 = 2001, x3 = 2011, predict_year = 2014){
+  # Calculate slope and intercept
+  years <- c(x1, x2, x3)
+  populations <- c(y1, y2, y3)
+  m <- sum((years - mean(years)) * (populations - mean(populations))) / sum((years - mean(years))^2)
+  b <- mean(populations) - m * mean(years)
+  
+  # Predict population for predict_year
+  return(m * predict_year + b)
+}
+
+# Use the function within mutate()
+df <- df %>%
+  rowwise() %>%
+  mutate(
+    pc96_pca_tot_p = approx(c(1991, 2001), c(pc91_pca_tot_p, pc01_pca_tot_p), xout = 1996)$y,
+    pc99_pca_tot_p = approx(c(1991, 2001), c(pc91_pca_tot_p, pc01_pca_tot_p), xout = 1999)$y,
+    pc04_pca_tot_p = approx(c(2001, 2011), c(pc01_pca_tot_p, pc11_pca_tot_p), xout = 2004)$y,
+    pc09_pca_tot_p = approx(c(2001, 2011), c(pc01_pca_tot_p, pc11_pca_tot_p), xout = 2009)$y,
+    pc14_pca_tot_p = predict_population(pc91_pca_tot_p, pc01_pca_tot_p, pc11_pca_tot_p)
+  ) %>%
+  ungroup()
+
+######################################
+
+df_list <- split(df, df$Year)
+
+df_1996 <- df_list[[1]]
+df_1999 <- df_list[[2]]
+df_2004 <- df_list[[3]]
+df_2009 <- df_list[[4]]
+
+df_vote_share_diff <- df_1996 %>%
+  group_by(district) %>%
+  reframe(vote_share_diff = Vote_Share_Percentage[Position == 1] - Vote_Share_Percentage[Position == 2])
+
+df_1996 <- left_join(df_1996, df_vote_share_diff, by = "district", relationship = "many-to-many")
+
+df_1996 <- df_1996 %>%
+  mutate(vote_share_diff = ifelse(Position == 2, -vote_share_diff, vote_share_diff),
+         population = pc96_pca_tot_p)
+
+###
+
+df_vote_share_diff <- df_1999 %>%
+  group_by(district) %>%
+  reframe(vote_share_diff = Vote_Share_Percentage[Position == 1] - Vote_Share_Percentage[Position == 2])
+
+df_1999 <- left_join(df_1999, df_vote_share_diff, by = "district", relationship = "many-to-many")
+
+df_1999 <- df_1999 %>%
+  mutate(vote_share_diff = ifelse(Position == 2, -vote_share_diff, vote_share_diff),
+         population = pc99_pca_tot_p)
+
+###
+
+df_vote_share_diff <- df_2004 %>%
+  group_by(district) %>%
+  reframe(vote_share_diff = Vote_Share_Percentage[Position == 1] - Vote_Share_Percentage[Position == 2])
+
+df_2004 <- left_join(df_2004, df_vote_share_diff, by = "district", relationship = "many-to-many")
+
+df_2004 <- df_2004 %>%
+  mutate(vote_share_diff = ifelse(Position == 2, -vote_share_diff, vote_share_diff),
+         population = pc04_pca_tot_p)
+
+###
+
+df_vote_share_diff <- df_2009 %>%
+  group_by(district) %>%
+  reframe(vote_share_diff = Vote_Share_Percentage[Position == 1] - Vote_Share_Percentage[Position == 2])
+
+df_2009 <- left_join(df_2009, df_vote_share_diff, by = "district", relationship = "many-to-many")
+
+df_2009 <- df_2009 %>%
+  mutate(vote_share_diff = ifelse(Position == 2, -vote_share_diff, vote_share_diff),
+         population = pc09_pca_tot_p)
+
+df <- bind_rows(df_1996, df_1999, df_2004, df_2009)
+
+unique(df$MyNeta_education)
+
+#########################
+
+education_levels <- c("Illiterate", "Literate", "5th Pass", "8th Pass", "10th Pass", "12th Pass", "Graduate", "Graduate Professional", "Post Graduate", "Doctorate")
+df$education <- factor(df$MyNeta_education, levels = education_levels, ordered = TRUE)
+df$education[df$education == "Others"] <- NA
+df$education <- as.numeric(df$education)
